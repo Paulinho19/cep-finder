ee6fc21a3dfbb41efb52d67ab1f2d4b64291ccff
Paulo Henrique <paulohsantosrocha@gmail.com>
2026-06-08 09:28:47 -0300

    first commit

---
+++ src/main/java/com/github/paulinho19/cep_finder/CepFinderApplication.java
@@ -0,0 +1,13 @@
+package com.github.paulinho19.cep_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/paulinho19/cep_finder/CepFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.paulinho19.cep_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
